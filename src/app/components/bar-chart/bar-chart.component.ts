2d510afd1dea6a6351dbf702f90e670a2f3bf2b8
IQ-shripal <shripal.garhwal@indoqubix.com>
2023-01-04 21:39:06 +0530

    Bar Chart component

--- src/app/components/bar-chart/bar-chart.component.ts
+++ src/app/components/bar-chart/bar-chart.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import * as echarts from 'echarts';
+
+type EChartsOption = echarts.EChartsOption;
+
+type BarChartConfig = {
+  title: string;
+  axisData: [];
+  seriesData: [];
+}
 
 @Component({
   selector: 'app-bar-chart',
@@ -6,5 +15,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./bar-chart.component.scss']
 })
 export class BarChartComponent {
+  @Input() config: BarChartConfig = {} as BarChartConfig
+  ngOnInit() {
+    const chartDom = document.getElementById('barChart');
+    if (chartDom) {
+      const myChart = echarts.init(chartDom);
+      let option: EChartsOption;;
 
+      option = {
+        title: {
+          text: "Patient Age"
+        },
+        xAxis: {
+          type: 'value'
+        },
+        yAxis: {
+          type: 'category',
+          data: ['40', '60', '80']
+        },
+        series: [
+          {
+            data: [120, 200, 150],
+            type: 'bar'
+          }
+        ]
+      };
+      option && myChart.setOption(option);
+    }
+  }
 }
