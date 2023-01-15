41f41a7e1320e65719ffb232fdee715f56e7c9ec
spider web <spideb.inc@gmail.com>
2023-01-15 18:27:30 +0530

    add changes

--- src/app/views/summary/summary.component.ts
+++ src/app/views/summary/summary.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare const $: any;
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.scss']
+  styleUrls: ['./summary.component.scss'],
 })
 export class SummaryComponent {
-
+  ngOnInit(): void {}
 }
