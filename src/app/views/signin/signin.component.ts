307e75429d54e17d0aae456bf22db85576f9f40c
spider web <spideb.inc@gmail.com>
2023-01-15 21:36:35 +0530

    signin page

--- src/app/views/signin/signin.component.ts
+++ src/app/views/signin/signin.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare const $: any;
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.scss']
+  styleUrls: ['./signin.component.scss'],
 })
 export class SigninComponent {
-
+  ngOnInit(): void {
+    $('.header').remove();
+    $('.footer').remove();
+    $('#back-to-top').remove();
+  }
 }
