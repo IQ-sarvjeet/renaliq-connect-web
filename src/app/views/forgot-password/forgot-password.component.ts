365a38ec3b4317d4341873dbe28d10f6b29bb524
Raghvendra Mishra <raghvendra.mishra@taazaa.com>
2023-01-30 22:36:51 +0530

    Forgot password code

--- src/app/views/forgot-password/forgot-password.component.ts
+++ src/app/views/forgot-password/forgot-password.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from 'src/app/api-client';
 declare const $: any;
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss'],
 })
-export class ForgotPasswordComponent {
+
+export class ForgotPasswordComponent implements OnInit{
+  form:any = FormGroup;
+  errorMsg :any ="";
+  successMsg :any ="";
+  fields :any ={email :1 ,mobile:2};
+  forgotPasswordModel :any ={
+    emailId:'',
+    mobileNumber:''
+  }
+  constructor(private _accountService: AccountService, private fb: FormBuilder){}
   ngOnInit(): void {
     $('.header').addClass('d-none');
     $('.footer').addClass('d-none');
     $('#back-to-top').addClass('d-none');
+    this.intializeform();
+  }
+  intializeform() {
+    this.form = this.fb.group({
+      emailId: ['', [Validators.required, Validators.email]],
+      mobileNumber: ['', [Validators.required]],
+    });
+  }
+
+  public async onSubmit(form: FormGroup) {
+    this.errorMsg="";
+    this.successMsg ="";
+    if (form.invalid) {
+      this.errorMsg="Email or Mobile Number is not valid.";
+      return;
+    }
+    let model: any = {
+      email: form.value.emailId,
+      phoneNumber : form.value.mobileNumber
+    };
+    try {
+      var result = await this._accountService.apiAccountLogoutPost().toPromise();
+      this.successMsg =" We have send a link to reset password."
+    } catch (ex: any) {
+      console.log(ex);
+      this.errorMsg=ex.error;
+    }
+  }
+  resetField(type:any){
+    if(type == this.fields.email){
+      this.form.controls['mobileNumber'].clearValidators(null);
+      this.form.controls['emailId'].setValidators([Validators.required, Validators.email]);
+      this.form.get('mobileNumber').patchValue("");
+      this.form.get('mobileNumber').markAsUntouched();
+    }
+    else if(type == this.fields.mobile){
+      this.form.controls['emailId'].clearValidators(null);
+      this.form.controls['mobileNumber'].setValidators(Validators.required);
+      this.form.get('emailId').patchValue("");
+      this.form.get('emailId').markAsUntouched();
+    }
+     this.form.controls['emailId'].updateValueAndValidity();
+     this.form.controls['mobileNumber'].updateValueAndValidity();
   }
   ngOnDestroy(): void {
     $('.header').removeClass('d-none');
--- src/app/views/reset-password/reset-password.component.ts
+++ src/app/views/reset-password/reset-password.component.ts
@@ -1,15 +1,61 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AccountService } from 'src/app/api-client';
 declare const $: any;
+let pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.scss'],
 })
-export class ResetPasswordComponent {
+export class ResetPasswordComponent implements OnInit{
+  resetForm:any = FormGroup;
+  errorMsg :any ="";
+  private token: string;
+  private email: string;
+
+  constructor(private _accountService: AccountService, private route: ActivatedRoute,private router: Router, private fb: FormBuilder){
+    this.token = this.route.snapshot.queryParams['token'];
+    this.email = this.route.snapshot.queryParams['email'];
+  }
   ngOnInit(): void {
     $('.header').addClass('d-none');
     $('.footer').addClass('d-none');
     $('#back-to-top').addClass('d-none');
+    this.intializeform();
+  } 
+  intializeform() {
+    this.resetForm = this.fb.group({
+      password: ['', [Validators.required,Validators.pattern(pattern)]],
+      confirmPassword: ['', [Validators.required,Validators.pattern(pattern)]],
+    });
+  }
+ 
+  public async onSubmit(form: FormGroup) {
+    this.errorMsg ="";
+    if (form.invalid) {
+      return;
+    }
+    
+    let password = form.get('password');
+    let confirmPassword = form.get('confirmPassword');
+    if(password != confirmPassword){
+      this.errorMsg = "Password and Confirm Password not matched."
+     return;
+    } 
+
+    let model: any = {
+      password: form.value.password,
+      token :  this.token
+    };
+    try {
+     // var result = await this._accountService.apiAccountLoginPost(model).toPromise();
+      this.router.navigate(['']);
+    } catch(ex:any) {
+      console.log(ex);
+      alert(ex.error?.error_description);
+    }
   }
   ngOnDestroy(): void {
     $('.header').removeClass('d-none');
@@ -17,3 +63,5 @@ export class ResetPasswordComponent {
     $('#back-to-top').removeClass('d-none');
   }
 }
+
+
