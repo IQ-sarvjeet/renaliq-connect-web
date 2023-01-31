be79f848b3ff046ae5a06eaf9214e0ace169b8bb
IQ-shripal <shripal.garhwal@indoqubix.com>
2023-01-31 21:41:17 +0530

    Created 2 factor authentication component

---
+++ src/app/views/two-fector-auth/two-fector-auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-two-fector-auth',
+  templateUrl: './two-fector-auth.component.html',
+  styleUrls: ['./two-fector-auth.component.scss']
+})
+export class TwoFectorAuthComponent {
+
+}
