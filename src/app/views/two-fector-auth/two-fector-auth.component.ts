f0f16037490ca20be1348ab3271763e71f435085
Shiv Singh <shivprakash@taazaa.com>
2023-02-05 00:39:16 +0530

    add 2fa login.

--- src/app/views/two-fector-auth/two-fector-auth.component.ts
+++ src/app/views/two-fector-auth/two-fector-auth.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { async } from 'rxjs';
+import { environment } from '../../../environments/environment';
 import { AccountService } from '../../api-client';
 import { CommonConstants } from '../../shared/common-constants/common-constants';
+import { setCookie } from '../../shared/services/cookie.service';
+import { HttpClientWapperService } from '../../shared/services/httpclient.wapper.service';
 import { LocalStorageService } from '../../shared/services/localstorage.service';
 declare const $: any;
 @Component({
@@ -15,9 +19,10 @@ export class TwoFectorAuthComponent {
 
   username: any = '';
   twoFACode: any = '';
-
+  password: any = '';
   constructor(private _localStorage: LocalStorageService,
     private _accountService: AccountService,
+    private _httpclientwapperSerivce: HttpClientWapperService,
     private route: Router) {
   }
 
@@ -36,10 +41,59 @@ export class TwoFectorAuthComponent {
     let userData = JSON.parse(jsonData);
     if (userData) {
       this.username = userData.username;
+      this.password = userData.password;
     }
     console.log(userData);
 
   }
+
+  public async onSubmit(form: any) {
+    if (form.invalid)
+      return;
+
+    await this.twoFALogin(form);
+
+  }
+
+  public async twoFALogin(form:any) {
+    let model: any = {
+      username: this.username,
+      twoFactorCode: this.twoFACode,
+      rememberMe: false
+    };
+
+    let data = await this._accountService.apiAccountAuthtokenValidatePost(model).subscribe(async (result: any) => {
+      if (result) {
+        this.login();
+      }
+    },
+      (error: any) => { console.log(error?.error?.message?.message); });
+
+  };
+
+  public async login() {
+    let model: any = {
+      username: this.username,
+      password: this.password,
+      grant_type: environment.grantType,
+      scope: environment.scope,
+      client_id: environment.clientId,
+      client_secret: environment.clientSecret,
+    };
+
+    var data = await this._httpclientwapperSerivce.apiAccountLoginPost(model).subscribe((result: any) => {
+      if (result) {
+        this._localStorage.setItem(CommonConstants.CONNECT_TOKEN_KEY, result.access_token);
+        setCookie(CommonConstants.CONNECT_TOKEN_KEY, result.access_token, CommonConstants.CONNECT_REFRESH_TOKEN_EXPIRY);
+        this.route.navigate(['']);
+      }
+    },
+      (error: any) => {
+        console.log(error?.error?.message?.message);
+      });
+  };
+
+
 
 
   ngOnDestroy(): void {
