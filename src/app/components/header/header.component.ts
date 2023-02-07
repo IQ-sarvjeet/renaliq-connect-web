7c4c1f014f5017be52804f98cab8a25e824546a6
IQ-shripal <shripal.garhwal@indoqubix.com>
2023-02-07 21:18:29 +0530

    Search patient implementation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { debounceTime, distinctUntilChanged, filter, fromEvent, tap } from 'rxjs';
 import { LocalStorageService } from 'src/app/shared/services/localstorage.service';
 import { AccountService } from '../../api-client';
 
@@ -9,12 +10,42 @@ import { AccountService } from '../../api-client';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  @ViewChild('searchInput') searchInput!: ElementRef;
+  patientList: any = [
+    {
+      name: "Ricky",
+      patientId: 485859
+    },
+    {
+      name: "Sachin",
+      patientId: 848544
+    },
+    {
+      name: "Mark",
+      patientId: 4858737
+    }
+  ]
+  patientSearchedList: any = [];
   constructor(
     private _accountService: AccountService,
     private _localStorage: LocalStorageService,
     private route: Router
   ) {}
-
+  ngAfterViewInit() {
+    fromEvent(this.searchInput.nativeElement,'keyup')
+      .pipe(
+          filter( (searchText: any) => {
+            if (searchText.target.value && searchText.target.value.length > 2) return searchText;
+          }),
+          debounceTime(500),
+          distinctUntilChanged(),
+          tap((text) => {})
+      )
+      .subscribe((searchText: any) => {
+        const text = searchText.target.value;
+        this.filterPatients(text);
+      });
+  }
   public async logOut() {
     try {
       var result = await this._accountService.apiAccountLogoutPost().toPromise();
@@ -23,5 +54,15 @@ export class HeaderComponent {
     } catch (ex: any) {
       console.log(ex);
     }
+  }
+  filterPatients(text: string) {
+    this.patientSearchedList = this.patientList.filter((item: any) => { 
+      return String(item.name).toLocaleLowerCase().indexOf(String(text).toLocaleLowerCase()) !== -1
+    });
+  }
+  patientSelectHandler(patient: any) {
+    this.searchInput.nativeElement.value = '';
+    this.patientSearchedList = [];
+    this.route.navigate(['/patient']);
   }
 }
