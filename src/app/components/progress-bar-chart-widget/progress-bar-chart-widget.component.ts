e954f797c92886a65f2e23e19a601a5169821976
IQ-shripal <shripal.garhwal@indoqubix.com>
2023-02-13 00:26:32 +0530

    Changed api url

--- src/app/components/progress-bar-chart-widget/progress-bar-chart-widget.component.ts
+++ src/app/components/progress-bar-chart-widget/progress-bar-chart-widget.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ProgressBarChartWidgetInput } from 'src/app/interfaces/progress-bar-chart-widget';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-progress-bar-chart-widget',
@@ -16,7 +17,7 @@ export class ProgressBarChartWidgetComponent {
     }
   }
   private loadChartData(url: string): void {
-    fetch(url).then((response: any) => response.json())
+    fetch(`${environment.baseApiUrl}api/${url}`).then((response: any) => response.json())
     .then((data: any) => {
       console.log('response:', data);
       this.chartData = data;
