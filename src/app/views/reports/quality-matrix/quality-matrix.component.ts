d50b846bb1c4aa144bcb328ce3d002ef785d0f81
Shiv Singh <shivprakash@taazaa.com>
2023-02-18 19:53:03 +0530

    connect-53: add api on quality report.

--- src/app/views/reports/quality-matrix/quality-matrix.component.ts
+++ src/app/views/reports/quality-matrix/quality-matrix.component.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ClinicalQualityMatrixService } from '../../../api-client';
 
 @Component({
   selector: 'app-quality-matrix',
   templateUrl: './quality-matrix.component.html',
   styleUrls: ['./quality-matrix.component.scss']
 })
-export class QualityMatrixComponent {}
+export class QualityMatrixComponent {
+
+  qualityMatircList: any = [];
+
+  constructor(private _qualityService: ClinicalQualityMatrixService,
+    private route: Router,) { }
+
+  ngOnInit() {
+    this.getQualityMatricList();
+  }
+
+
+  getQualityMatricList() {
+
+    this._qualityService.apiClinicalQualityMatrixGetGet().subscribe((result: any) => {
+      this.qualityMatircList = result.clinicalQualityMatrics;
+      console.log(result);
+    },
+      (error) => {
+
+      });
+  }
+
+}
