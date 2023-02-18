f3a5889795174542a69dcd0be9ab07ccbf0d286c
IQ-shripal <shripal.garhwal@indoqubix.com>
2023-02-18 23:02:42 +0530

    Api integration

--- src/app/views/reports/quality-matrix/quality-matrix.component.ts
+++ src/app/views/reports/quality-matrix/quality-matrix.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { BarChartConfig } from 'src/app/interfaces/bar-chart-config';
 
 @Component({
   selector: 'app-quality-matrix',
   templateUrl: './quality-matrix.component.html',
   styleUrls: ['./quality-matrix.component.scss']
 })
-export class QualityMatrixComponent {}
+export class QualityMatrixComponent {
+  chartConfig: BarChartConfig = {
+    apiUrl: 'assets/mockData/chartQualityMatrix.json',
+    title: '',
+    colors: ['#c4c4c4', '#448c00', '#ff6708', '#ff3700']
+  }
+  openChartModal: boolean = false;
+  openChart(){
+    this.openChartModal = true;
+    setTimeout(() => {
+      this.openChartModal = false;
+    }, 2000);
+  }
+  modalClosed(){
+    this.openChartModal = false;
+  }
+}
