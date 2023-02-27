004d8c8dcafc16a34dfe10db47c0bc20c506bcbf
S Choudhary <schoudhary@indoqubix.com>
2023-02-27 11:29:26 +0530

    update conf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,10 +4,12 @@
 
 export const environment = {
   production: false,
-  // baseApiUrl: 'https://renaliq-comm-api-dev-connect.azurewebsites.net/',
-  baseApiUrl: 'https://localhost:5001',
+  baseApiUrl: 'https://renaliq-comm-api-dev-connect.azurewebsites.net/',
+ // baseApiUrl: 'https://localhost:5001',
   appUrl: 'https://localhost:4200',
   pageSize: 10,
+  clientId: 'WebUI.Admin',
+  clientSecret: 'eNuscFFwA8X4tJEYsQyjndUkrQhxN3Xu'
   //Username: tom@yopmail.com
   //Password: Pass@12345
 };
