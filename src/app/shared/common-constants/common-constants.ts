66ea8f6c7fc15abb2e4bdcdcb9126da53ee181d5
Shiv Singh <shivprakash@taazaa.com>
2023-03-02 22:30:29 +0530

    fixed issue - 206,183,197.

--- src/app/shared/common-constants/common-constants.ts
+++ src/app/shared/common-constants/common-constants.ts
@@ -4,6 +4,7 @@ export class CommonConstants {
   public static CONNECT_REFRESH_TOKEN_KEY: string = "refresh_tk";
   public static CONNECT_REFRESH_TOKEN_EXPIRY: string = "0.5";
   public static TWO_FA_KEY: string = "twoFA_login";
+  public static USER_INFO_KEY: string = "user-info";
 
 
 
