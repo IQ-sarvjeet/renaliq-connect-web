0857cc3ae23bed4404090a7e842dcf778025c3b9
IQ-shripal <shripal.garhwal@indoqubix.com>
2023-03-11 14:04:51 +0530

    Added valid user condition in Auth Gaurd

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
+import { UserInfo } from '../interfaces/user';
 import { CommonConstants } from '../shared/common-constants/common-constants';
 import { LocalStorageService } from '../shared/services/localstorage.service';
+import { StoreService } from './store.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private _localStorage: LocalStorageService) { }
+  constructor(private _localStorage: LocalStorageService, private storeService: StoreService) { }
   isLoggedIn() {
+    const userInfo: UserInfo = this.storeService.getUserInfo();
+    if(!userInfo || !userInfo.fullName) {
+      return false;
+    }
     return !!this._localStorage.getItem(CommonConstants.CONNECT_TOKEN_KEY);
   }
 }
