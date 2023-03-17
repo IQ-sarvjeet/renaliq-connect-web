0efb6a91dbfbcb4b9257e0a200547ef76ba2b7a8
IQ-shripal <shripal.garhwal@indoqubix.com>
2023-03-17 21:34:16 +0530

    Patient profile

--- src/app/views/patient/patient-list/patient-list.component.ts
+++ src/app/views/patient/patient-list/patient-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import * as moment from 'moment';
 import { PatientService } from 'src/app/api-client';
 declare var $:any;
 
@@ -9,24 +10,37 @@ declare var $:any;
   styleUrls: ['./patient-list.component.scss']
 })
 export class PatientListComponent {
+  moment = moment;
+  carePlans: any = [];
   patientDetail: any = {};
   constructor(private route: Router, private patientService: PatientService) {}
   actionHandler($event: any) {
     this.patientDetail = $event.detail;
     if($event.actionType === 'viewCarePlan') {
       $('#carePlanFilter').modal('show');
-      this.patientService.apiPatientCareplansEnrollmentNumberGet($event.detail.patient.enrollmentNo).subscribe({
+      // this.patientService.apiPatientCareplansEnrollmentNumberGet($event.detail.patient.enrollmentNo).subscribe({
+      //   next: (response: any) => {
+      //     console.log('response:', response);
+      //   }
+      // })
+      this.patientService.apiPatientCareplansEnrollmentNumberGet('ZXDY06257796').subscribe({
         next: (response: any) => {
+          this.carePlans = response;
           console.log('response:', response);
         }
       })
     } else {
-      // this.route.navigate([`patient-profile/${this.patientDetail.patient.patientId}`]);
-      // this.route.navigate([`patient-profile`], );
       this.route.navigateByUrl(`/patient-profile/${this.patientDetail.patient.patientId}`, {state: {
         patientId: this.patientDetail.patient.patientId,
         enrollmentNo: this.patientDetail.patient.enrollmentNo
       }})
     }
+  }
+  downloadPlan(plan: any) {
+    this.patientService.apiPatientCareplanDownloadPatientActivityIdGet(plan.patientActivityId).subscribe({
+      next: (response: any) => {
+        
+      }
+    })
   }
 }
