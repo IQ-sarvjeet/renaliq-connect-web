7e27a80e6311c1b161a0c1934b2a88948baf8fcf
IQ-shripal <shripal.garhwal@indoqubix.com>
2023-03-19 20:23:50 +0530

    Integrated Medication and care member api

--- src/app/views/patient-profile/medication/medication.component.ts
+++ src/app/views/patient-profile/medication/medication.component.ts
@@ -12,37 +12,14 @@ export class MedicationComponent {
       this.loadMedication(value.enrollmentNo);
     }
   }
-  medications: any = [
-    {
-      id: 3345,
-      drugName: "Albuterol Sulfate",
-      unit: "mg",
-      strength: "2",
-      form: "pill",
-      delivery: "PO",
-      specialInstruction: "",
-      frequency: null,
-      discontinued: false
-    },
-    {
-      id: 3350,
-      drugName: "Cetirizine ",
-      unit: "mg",
-      strength: "10 ",
-      form: "pill",
-      delivery: "po",
-      specialInstruction: "",
-      frequency: null,
-      discontinued: false
-    }
-  ]
+  medications: any = []
   constructor(private patientService: PatientService) {
     
   }
   loadMedication(value: string) {
     this.patientService.apiPatientMedicationEnrollmentNumberGet(value).subscribe({
       next: (response: any) => {
-
+        this.medications = response;
       }
     })
   }
