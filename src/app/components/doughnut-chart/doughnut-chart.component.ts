e7308a400bde6cf18a448aa4f3ed254110f65d93
IQ-shripal <shripal.garhwal@indoqubix.com>
2023-04-13 20:28:35 +0530

    Reverted echarts changes

--- src/app/components/doughnut-chart/doughnut-chart.component.ts
+++ src/app/components/doughnut-chart/doughnut-chart.component.ts
@@ -3,10 +3,6 @@ import { Component, Input } from '@angular/core';
 // import * as Highcharts from 'highcharts';
 import { Messages } from 'src/app/shared/common-constants/messages';
 import { environment } from 'src/environments/environment';
-import { ElementRef, ViewChild } from '@angular/core';
-import * as echarts from 'echarts';
-
-type EChartsOption = echarts.EChartsOption;
 
 type BarChartConfig = {
   apiUrl: string;
@@ -18,7 +14,7 @@ type BarChartConfig = {
   styleUrls: ['./doughnut-chart.component.scss']
 })
 export class DoughnutChartComponent {
-  @ViewChild('doughnutChart', { static: false }) doughnutChart!: ElementRef<HTMLDivElement>;
+  // @ViewChild('doughnutChart', { static: false }) doughnutChart!: ElementRef<HTMLDivElement>;
   showLoading: boolean = false;
   errorMessage: string | null = null;
   // Highcharts = Highcharts;
@@ -26,52 +22,52 @@ export class DoughnutChartComponent {
     this.chartConfig = inputValue;
     this.fetchChartData(inputValue.apiUrl);
   }
-  // options: any = {
-  //   chart: {
-  //     type: 'pie',
-  //   },
-  //   title: {
-  //     text: '',
-  //     align: 'left'
-  //   },
-  //   subtitle: {
-  //     text: '',
-  //     align: 'left'
-  //   },
-  //   plotOptions: {
-  //     pie: {
-  //       dataLabels: {
-  //         enabled: false,
-  //         style: {
-  //           fontWeight: 'bold',
-  //           color: 'black'
-  //         }
-  //       },
-  //       center: ['35%', '55%'],
-  //       size: '130%'
-  //     }
-  //   },
-  //   tooltip: {
-  //     enabled: true,
-  //     style:{
-  //       color: '#333333',
-  //       cursor: 'default',
-  //       fontSize: '12px',
-  //       whiteSpace: 'nowrap',
-  //     }
-  //   },
-  //   legend: {
-  //     align: 'right',
-  //     layout: 'vertical'
-  //   },
-  //   colors: ["#76ADDB", "#C8DB70", "#0B314F", "#999999", "#d96716"],
-  //   series: [{
-  //     name: '',
-  //     innerSize: '50%',
-  //     data: [],
-  //     showInLegend: true
-  //   }]
-  // }
+  options: any = {
+    chart: {
+      type: 'pie',
+    },
+    title: {
+      text: '',
+      align: 'left'
+    },
+    subtitle: {
+      text: '',
+      align: 'left'
+    },
+    plotOptions: {
+      pie: {
+        dataLabels: {
+          enabled: false,
+          style: {
+            fontWeight: 'bold',
+            color: 'black'
+          }
+        },
+        center: ['35%', '55%'],
+        size: '130%'
+      }
+    },
+    tooltip: {
+      enabled: true,
+      style:{
+        color: '#333333',
+        cursor: 'default',
+        fontSize: '12px',
+        whiteSpace: 'nowrap',
+      }
+    },
+    legend: {
+      align: 'right',
+      layout: 'vertical'
+    },
+    colors: ["#76ADDB", "#C8DB70", "#0B314F", "#999999", "#d96716"],
+    series: [{
+      name: '',
+      innerSize: '50%',
+      data: [],
+      showInLegend: true
+    }]
+  }
   private chartConfig: BarChartConfig = {} as BarChartConfig;
   constructor(private httpClient: HttpClient) {
 
@@ -103,65 +99,17 @@ export class DoughnutChartComponent {
     })
   }
   private renderChart(chartData: any): void {
-    // const series = this.options.series;
-    // series[0].data = chartData;
-    // this.options = {
-    //   ...this.options,
-    //   title: {
-    //     ...this.options.title,
-    //     text: this.chartConfig.title 
-    //   },
-    //   series: [
-    //     ...series
-    //   ]
-    // }
-
-
-
-
-    const chartEle = this.doughnutChart.nativeElement;
-    if (chartEle) {
-      const chartRef = echarts.init(chartEle);
-      let option: EChartsOption;
-      option = {
-        color: ["#76ADDB", "#C8DB70", "#0B314F", "#999999", "#d96716"],
-        title: {
-          text: this.chartConfig.title,
-        },
-        tooltip: {
-          trigger: 'item'
-        },
-        legend: {
-          top: '5%',
-          right: '5%',
-          width: '30%',
-        },
-        series: [
-          {
-            name: 'Access From',
-            type: 'pie',
-            radius: ['50%', '90%'],
-            avoidLabelOverlap: false,
-            left: '-50%',
-            label: {
-              show: false,
-              position: 'center'
-            },
-            emphasis: {
-              label: {
-                show: true,
-                fontSize: 15,
-                fontWeight: 'bold'
-              }
-            },
-            labelLine: {
-              show: false
-            },
-            data: chartData
-          }
-        ]
-      }
-      option && chartRef.setOption(option);
+    const series = this.options.series;
+    series[0].data = chartData;
+    this.options = {
+      ...this.options,
+      title: {
+        ...this.options.title,
+        text: this.chartConfig.title 
+      },
+      series: [
+        ...series
+      ]
     }
   }
 }
