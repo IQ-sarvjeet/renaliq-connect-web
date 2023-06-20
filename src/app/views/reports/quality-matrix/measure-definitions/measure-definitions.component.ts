38c89482604cef770b2757ed2a4ee850e0adea2c
IQ-Bharti <bharti@indoqubix.com>
2023-06-20 17:14:40 +0530

    integrated measure definition api

--- src/app/views/reports/quality-matrix/measure-definitions/measure-definitions.component.ts
+++ src/app/views/reports/quality-matrix/measure-definitions/measure-definitions.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClinicalQualityMatrixService } from 'src/app/api-client';
 import { EventService } from 'src/app/services/event.service';
 import { ReportEventService } from '../../services/report-event.service';
 declare var $: any;
@@ -9,8 +10,11 @@ declare var $: any;
   styleUrls: ['./measure-definitions.component.scss']
 })
 export class MeasureDefinitionsComponent {
+  metricDefinition: any;
+
   constructor(private eventService: EventService,
-    private reportService: ReportEventService) { }
+    private reportService: ReportEventService,
+    private _qualityService: ClinicalQualityMatrixService) { }
 
   ngOnInit() {
     this.eventService.openModalSubscription().subscribe(([openModal, isGlobal]: [boolean, boolean]) => {
@@ -21,6 +25,13 @@ export class MeasureDefinitionsComponent {
 
     this.reportService.metricIdSubscription().subscribe((metricID: number) => {
       if(metricID) {
+        this._qualityService.apiClinicalQualityMatrixMetricDetailsIdGet(metricID).subscribe({
+          next: (response: any) => {
+            this.metricDefinition = response;
+          },
+          error: (error: any) => {
+          }
+        });
       }
     })
   }
