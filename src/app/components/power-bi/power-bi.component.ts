465f5c3dbe0a36baebf258edf3063ef59221408b
IQ-Bharti <bharti@indoqubix.com>
2023-06-21 11:58:38 +0530

    integrated powerBI local api

--- src/app/components/power-bi/power-bi.component.ts
+++ src/app/components/power-bi/power-bi.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-power-bi',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./power-bi.component.scss']
 })
 export class PowerBIComponent {
+  embedConfig!: any;
+  @Input() config: any;
 
+  ngOnInit(): void {
+    this.embedConfig = {
+      type: 'report',
+      id: this.config.id,
+      embedUrl: this.config.embedUrl,
+      accessToken: this.config.embedToken.token,
+      // tokenType: models.TokenType.Embed,
+      hostname: 'https://app.powerbi.com',
+      settings: {
+        panes: {
+          filters: {
+            expanded: false,
+            visible: false,
+          },
+        },
+        // background: models.BackgroundType.Transparent,
+      },
+    };
+  }
 }
--- src/app/views/reports/power-bi-report/power-bi-report.component.ts
+++ src/app/views/reports/power-bi-report/power-bi-report.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-power-bi-report',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./power-bi-report.component.scss']
 })
 export class PowerBIReportComponent {
+  configData: any;
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit() {
+    this.getPowerBIReport();
+  }
 
+  async getPowerBIReport(){
+    this.httpClient.get('https://localhost:5001/api/Report/report?reporturi=ckcc').subscribe({
+      next: (reportData: any) => {
+        this.configData = reportData;
+      },
+      error: (error: any) => {},
+    });
+  }
 }
