886dc2ff3b91d20f00ea8f1a32f2efaed03ec13f
IQ-apathak <ashish.pathak@indoqubix.com>
2023-08-02 18:49:02 +0530

    google analytics

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { CommonConstants } from 'src/app/shared/common-constants/common-constants';
 import { Messages } from 'src/app/shared/common-constants/messages';
 
 @Component({
@@ -6,6 +7,10 @@ import { Messages } from 'src/app/shared/common-constants/messages';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
   messages: any = Messages;
+  gtagId! : string;
+  ngOnInit() {
+    this.gtagId = CommonConstants.GTAG_ID;
+ }
 }
--- src/app/shared/common-constants/common-constants.ts
+++ src/app/shared/common-constants/common-constants.ts
@@ -6,6 +6,7 @@ export class CommonConstants {
   public static TWO_FA_KEY: string = "twoFA_login";
   public static USER_INFO_KEY: string = "user-info";
   public static EXPIRATION_TIME: string = "expiration_time"
+  public static GTAG_ID: string = "G-ZMXS65JS1G";
 
 
 
