f27e81fa04c82911e41dc73ae2aee591385cbaf1
IQ-apathak <ashish.pathak@indoqubix.com>
2023-08-07 12:55:36 +0530

    append the version

--- src/app/views/signup/signup.component.ts
+++ src/app/views/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SystemService } from 'src/app/api-client/api/system.service';
+import { CommonConstants } from 'src/app/shared/common-constants/common-constants';
 import { Messages } from 'src/app/shared/common-constants/messages';
 declare const $: any;
 @Component({
@@ -8,10 +10,26 @@ declare const $: any;
 })
 export class SignupComponent {
   messages: any = Messages;
+  apiVersion!: string;
+  webVersion!: string;
+  constructor(private systemService: SystemService) {}
   ngOnInit(): void {
     $('.header').addClass('d-none');
     $('.footer').addClass('d-none');
     $('#back-to-top').addClass('d-none');
+    this.getVersion();
+    this.webVersion = CommonConstants.WEB_VERSION;
+  }
+  private getVersion(){
+    this.systemService.apiVersionGet().subscribe(
+      (response: any) => {
+        this.apiVersion = response.version;
+      },
+      (error) => {
+        //console.error('Error fetching API version:', error);
+        this.apiVersion = 'Unknown';
+      }
+    );
   }
   ngOnDestroy(): void {
     $('.header').removeClass('d-none');
