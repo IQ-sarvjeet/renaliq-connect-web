dd3d0205fe33d7322de22166a9cf1824bb57b2a2
IQ-Bharti <bharti@indoqubix.com>
2023-08-08 10:31:04 +0530

    generated swagger files

--- src/app/api-client/api/notification.service.ts
+++ src/app/api-client/api/notification.service.ts
@@ -18,6 +18,7 @@ import { CustomHttpUrlEncodingCodec }                        from '../encoder';
 import { Observable }                                        from 'rxjs';
 
 import { NotificationListRequestModel } from '../model/notificationListRequestModel';
+import { UpdateNotificationMediumRequest } from '../model/updateNotificationMediumRequest';
 
 import { BASE_PATH, COLLECTION_FORMATS }                     from '../variables';
 import { Configuration }                                     from '../configuration';
@@ -139,6 +140,58 @@ export class NotificationService {
         }
 
         return this.httpClient.request<any>('post',`${this.basePath}/api/Notification/list`,
+            {
+                body: body,
+                withCredentials: this.configuration.withCredentials,
+                headers: headers,
+                observe: observe,
+                reportProgress: reportProgress
+            }
+        );
+    }
+
+    /**
+     * 
+     * 
+     * @param body 
+     * @param observe set whether or not to return the data Observable as the body, response or events. defaults to returning the body.
+     * @param reportProgress flag to report request and response progress.
+     */
+    public apiNotificationUpdateNotifyMediumPut(body?: UpdateNotificationMediumRequest, observe?: 'body', reportProgress?: boolean): Observable<any>;
+    public apiNotificationUpdateNotifyMediumPut(body?: UpdateNotificationMediumRequest, observe?: 'response', reportProgress?: boolean): Observable<HttpResponse<any>>;
+    public apiNotificationUpdateNotifyMediumPut(body?: UpdateNotificationMediumRequest, observe?: 'events', reportProgress?: boolean): Observable<HttpEvent<any>>;
+    public apiNotificationUpdateNotifyMediumPut(body?: UpdateNotificationMediumRequest, observe: any = 'body', reportProgress: boolean = false ): Observable<any> {
+
+
+        let headers = this.defaultHeaders;
+
+        // authentication (Bearer) required
+        if (this.configuration.accessToken) {
+            const accessToken = typeof this.configuration.accessToken === 'function'
+                ? this.configuration.accessToken()
+                : this.configuration.accessToken;
+            headers = headers.set('Authorization', 'Bearer ' + accessToken);
+        }
+        // to determine the Accept header
+        let httpHeaderAccepts: string[] = [
+        ];
+        const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+        if (httpHeaderAcceptSelected != undefined) {
+            headers = headers.set('Accept', httpHeaderAcceptSelected);
+        }
+
+        // to determine the Content-Type header
+        const consumes: string[] = [
+            'application/json',
+            'text/json',
+            'application/_*+json'
+        ];
+        const httpContentTypeSelected: string | undefined = this.configuration.selectHeaderContentType(consumes);
+        if (httpContentTypeSelected != undefined) {
+            headers = headers.set('Content-Type', httpContentTypeSelected);
+        }
+
+        return this.httpClient.request<any>('put',`${this.basePath}/api/Notification/update/notify-medium`,
             {
                 body: body,
                 withCredentials: this.configuration.withCredentials,
