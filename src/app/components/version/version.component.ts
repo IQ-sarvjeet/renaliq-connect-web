55fc580efe78c42dd76cd04c60f15896efd42918
IQ-Bharti <bharti@indoqubix.com>
2023-08-09 13:06:28 +0530

    created new component for app version

---
+++ src/app/components/version/version.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { SystemService } from 'src/app/api-client';
+import { CommonConstants } from 'src/app/shared/common-constants/common-constants';
+
+@Component({
+  selector: 'app-version',
+  templateUrl: './version.component.html',
+  styleUrls: ['./version.component.scss']
+})
+export class VersionComponent implements OnInit{
+  apiVersion!: string;
+  webVersion!: string;
+  constructor(private systemService: SystemService){}
+  ngOnInit(): void {
+    this.webVersion = CommonConstants.WEB_VERSION;
+    this.getVersion();
+  }
+  getVersion(){
+    this.systemService.apiVersionGet().subscribe({
+      next: (response: any) => {
+        if (response.version) {
+          this.apiVersion = response.version
+        }
+      },
+      error: (error: any) => {
+      }
+    });
+  }
+}
