e34b7c3e1be14693ec37e60c63a41271bf0e3471
IQ-sarvjeet <142384561+IQ-sarvjeet@users.noreply.github.com>
2023-09-08 19:37:53 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  baseApiUrl: 'https://renaliq-comm-api-dev-connect.azurewebsites.net',
-  appUrl: 'https://agreeable-forest-060cbdb10.2.azurestaticapps.net',
+  baseApiUrl: __baseApiUrl__,
+  appUrl: __appUrl__,
   identity: {
     clientId: 'WebUI.Admin',
     clientSecret: 'eNuscFFwA8X4tJEYsQyjndUkrQhxN3Xu',
